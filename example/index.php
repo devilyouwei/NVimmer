d74eb4e67474d49a2ec39de33571407117e9eca4
devilyouwei <devilyouwei@gmail.com>
2020-02-18 17:54:17 -0500

    modify gitignore

--- example/index.php
+++ example/index.php
@@ -4,19 +4,21 @@ echo $_GET['username'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<style></style>
-<script>
-s
-</script>
+    <style></style>
+    <script>
+    </script>
 </head>
+
 <body>
-<form action="index.php" method="POST">
-<input type="text" name="username">
-<input type="submit" value="submit">
-</form>
+    <form action="index.php" method="POST">
+        <input type="text" name="username">
+        <input type="submit" value="submit">
+    </form>
 </body>
+
 </html>
