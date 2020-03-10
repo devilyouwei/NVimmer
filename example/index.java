2fe8902877174d538389bf778242211aa668e025
devilyouwei <devilyouwei@gmail.com>
2020-03-10 18:39:20 -0400

    remove unused examples

--- example/index.java
+++
@@ -1,6 +0,0 @@
-public class index {
-    int x;
-
-    public static void main(String[] args) {
-    }
-}
--- example/index.js
+++
@@ -1,9 +0,0 @@
-function test() {
-    document.getElementById(elementId)
-    window.localStorage.getItem(key)
-
-    let i = 1
-    let i = 1
-    for (let s of sequence) {
-    }
-}
