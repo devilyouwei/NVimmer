3f6e4ab84ac753cbc25563111995f8a5d98b8f9f
devilyouwei <devilyouwei@gmail.com>
2020-04-04 00:36:18 -0400

    fix curl in the shell script

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+echo " _   ___     ___                               "
+echo "| \\ | \\ \\   / (_)_ __ ___  _ __ ___   ___ _ __ "
+echo "|  \\| |\\ \\ / /| | '_ \` _ \\| '_ \` _ \\ / _ \\ '__|"
+echo "| |\\  | \\ V / | | | | | | | | | | | |  __/ |   "
+echo "|_| \\_|  \\_/  |_|_| |_| |_|_| |_| |_|\\___|_|"
+
 echo "Welcome to NVimmer, Make your NeoVim/Vim perform as IDE"
+echo "Email: devilyouwei@gmail.com"
+echo "Waiting for installing, 3 seconds..."
+
+sleep 3
+
+echo "Check snap------------------------------------------------"
+(snap --version)
+status=$?
+if [ "$status" != 0 ]; then
+    echo "Install Snap..."
+    (sudo apt install snapd)
+fi
 
 echo "Check NeoVim----------------------------------------------"
 (nvim -v)
 status=$?
-if [ "$status" -eq 0 ];then
+if [ "$status" -eq 0 ]; then
     echo "NeoVim found, next step..."
 else
     echo "Install NeoVim..."
@@ -15,7 +33,7 @@ fi
 echo "Check Node.js---------------------------------------------"
 (node -v)
 status=$?
-if [ "$status" -eq 0 ];then
+if [ "$status" -eq 0 ]; then
     echo "NodeJS found, next step..."
 else
     echo "Install Node.js..."
@@ -23,7 +41,15 @@ else
 fi
 
 echo "Install ESLint--------------------------------------------"
-npm -g install eslint neovim
+(eslint -v)
+status=$?
+if [ "$status" -eq 0 ]; then
+    echo "ESlint found, next step..."
+else
+    echo "Instal eslint..."
+    (npm -g install eslint)
+fi
+npm -g install neovim
 
 echo "Install Astyle--------------------------------------------"
 sudo apt install astyle
@@ -41,6 +67,7 @@ cp ./.eslintrc.json ~/
 cp ./.prettierrc.json ~/
 
 echo "Install vim-plug------------------------------------------"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim -c PlugInstall
